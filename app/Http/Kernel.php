a95668f3db9e34261e653ba8926d44f12fb91481
cenhuqing <cenhuqing@gmail.com>
2018-09-29 11:44:15 +0800

    前后台登录完成

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AuthAdmin;
+use App\Http\Middleware\GuestAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -60,5 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth.admin' => AuthAdmin::class,
+        'guest.admin' => GuestAdmin::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// 后台
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middleware' => 'auth.admin'], function () {
+        Route::get('/', 'Admin\IndexController@index');
+    });
+
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login');
+    Route::post('logout', 'Admin\LoginController@logout');
+});
